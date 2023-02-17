3168d5f6e6a3965f35391425c139817d300d62c8
tachiyu <yutoforgit@gmail.com>
2023-02-17 16:15:57 +0900

    Delete a commented out part

--- app/src/main/java/com/example/myLifeLog/App.kt
+++ app/src/main/java/com/example/myLifeLog/App.kt
@@ -10,33 +10,3 @@ fun App(viewModel: MainViewModel) {
         navController = navController)
 }
 
-//@Composable
-//fun NavBottomBar(navController: NavHostController) {
-//    val tabs = DESTINATIONS.values().slice(0..1)
-//    val navBackStackEntry by navController.currentBackStackEntryAsState()
-//    val currentRoute = navBackStackEntry?.destination?.route
-//    var cnt by remember{ mutableStateOf(0) } // 設定画面に映るためのカウンタ（隠しコマンド）
-//
-//    BottomNavigation (
-//        modifier = Modifier,
-//            ){
-//        tabs.forEach { tab ->
-//            BottomNavigationItem(
-//                selected = tab.str == currentRoute,
-//                onClick = {
-//                    if (tab.str != currentRoute) {
-//                        navController.navigate(tab.str)
-//                    } else /*同じタブを20回押すと設定画面に*/{
-//                        if (cnt >= 20) {
-//                            navController.navigate(DESTINATIONS.SETTING.str)
-//                            cnt = 0
-//                        } else {
-//                            cnt ++
-//                        }
-//                    }
-//                },
-//                icon = {},
-//                label = {Text(text = stringResource(tab.shown))})
-//        }
-//    }
-//}
